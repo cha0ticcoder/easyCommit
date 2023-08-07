80457d4cb97410ed8d613bd294e9c2ac2e337ab6
cha0tic.js <120225615+cha0ticcoder@users.noreply.github.com>
2023-08-07 13:33:33 +0200

    Update commitToGitHub.bat
    Better Script

--- commitToGitHub.bat
+++ commitToGitHub.bat
@@ -1,15 +1,19 @@
 @echo off
 
-:COMMIT-MESSAGE
+:: Here you are going to type the commit Message
 
-set CommitMessage="a"
+set CommitMessage="Example!"
 ECHO Type a commit Message (required)
 set /p CommitMessage=
+
+:: Press Button to commit & push to GitHub
 
 pause
 
 git add .
 git commit -m "%CommitMessage%"
 git push
+
+:: Getting paused if any errors or something occurs for you to solve :)
 
 pause
